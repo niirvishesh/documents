382bc3c4546bfde62e839c5a92685c0d902dec3b
Nirvishesh Shrivastava <31612080+niirvishesh@users.noreply.github.com>
2018-04-05 00:23:19 +0530

    Add files via upload

---
+++ js/coming-soon.js
@@ -0,0 +1,12 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Vide - Video Background Settings
+  $('body').vide({
+    mp4: "mp4/bg-1.mp4",
+    poster: "img/bg-1.jpg"
+  }, {
+    posterType: 'jpg'
+  });
+
+})(jQuery); // End of use strict
